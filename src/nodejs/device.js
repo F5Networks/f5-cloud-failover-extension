f2ff7bdb5ec3598de52a4f10e8f14285182c5670
Andrey KASHCHEEV <a.kashcheev@f5.com>
2019-07-10 16:26:46 -0700

    add device.js module as well as unit tests for this module

---
+++ src/nodejs/device.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2018 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const q = require('q');
+const Logger = require('./logger.js');
+const util = require('./util.js');
+const logger = new Logger(module);
+
+const f5CloudLibs = require('@f5devcentral/f5-cloud-libs');
+const BigIp = f5CloudLibs.bigIp;
+const bigip = new BigIp({ logger });
+
+
+class Device{
+
+    constructor(hostname, username, password, mgmtPort) {
+        //this.initialize(hostname, username, password, mgmtPort);
+        this.hostname = hostname;
+        this.username = username;
+        this.password = password;
+        this.mgmtPort = mgmtPort;
+    }
+
+    initialize(hostname, username, password, mgmtPort){
+
+        return bigip.init(
+            hostname,
+            username,
+            password,
+            {
+                port: mgmtPort,
+                product: 'BIG-IP'
+            }
+        ).then( () => {
+            logger.info('BIG-IP has been initialized');
+            return getConfig();
+        })
+        .then((results) => {
+            initFailoverConfig(results)
+        })
+        .catch((err) => {
+            logger.error(`device.initialize() error: ${util.stringify(err.message)}`);
+            return Promise.reject(err);
+        });
+    }
+
+    getConfig(){
+        return Promise.all([
+            bigip.list('/tm/sys/global-settings'),
+            bigip.list('/tm/cm/traffic-group/stats'),
+            bigip.list('/tm/net/self'),
+            bigip.list('/tm/ltm/virtual-address')
+        ]);
+    }
+
+    initFailoverConfig(results){
+
+        this.globalSettings = results[0];
+        this.trafficGroups = results[1];
+        this.selfAddresses = results[2];
+        this.virtualAddresses = results[3];
+
+        logger.info('BIG IP Failover configuration has been initialized.');
+    }
+
+    getGlobalSettings(){
+        return this.globalSettings;
+    }
+
+    getTrafficGroupsStats(){
+         return this.trafficGroups;
+    }
+
+    getSelfAddresses(){
+        return this.selfAddresses;
+    }
+
+    getVirtualAddresses(){
+        return this.virtualAddresses;
+    }
+
+}
+
+
+module.exports = Device;
---
+++ test/unittests/deviceTest.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2018 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+
+const assert = require('assert');
+const Device = require('../../src/nodejs/device');
+const nock = require('nock');
+
+const mockResults = [
+    "globalSettings",
+    "trafficGroups",
+    "selfAddresses",
+    "virtualAddresses"
+]
+
+let device;
+
+
+describe('device', () => {
+
+    beforeEach(() => {
+
+       nock(/.*/)
+          .persist()
+          .get(/.*/)
+          .reply(200, "Recieved")
+          .post(/.*/)
+          .reply(201, "Created")
+
+        device = new Device('localhost', 'admin', 'admin', '443');
+        device.initFailoverConfig(mockResults)
+    });
+
+
+    it('validate initialize', (done) => {
+        device.initialize('localhost', 'admin', 'admin', '443').then((response) => {
+
+
+        });
+        assert.equal("Created", "Created");
+        done();
+    });
+
+    it('validate initFailoverConfig', () => {
+        device.initFailoverConfig(mockResults)
+        assert.strictEqual(device.globalSettings, "globalSettings");
+        assert.strictEqual(device.trafficGroups, "trafficGroups");
+        assert.strictEqual(device.selfAddresses, "selfAddresses");
+        assert.strictEqual(device.virtualAddresses, "virtualAddresses");
+    });
+
+    it('validate getGlobalSettings', () => {
+        assert.strictEqual(device.getGlobalSettings(), "globalSettings");
+    });
+
+    it('validate getTrafficGroupsStats', () => {
+        assert.strictEqual(device.getTrafficGroupsStats(), "trafficGroups");
+    });
+
+    it('validate getSelfAddresses', () => {
+        assert.strictEqual(device.getSelfAddresses(), "selfAddresses");
+    });
+
+    it('validate getVirtualAddresses', () => {
+        assert.strictEqual(device.getVirtualAddresses(), "virtualAddresses");
+    });
+
+});
