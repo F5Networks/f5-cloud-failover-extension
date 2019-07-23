d0ce6a9388b86c8685db48c153178d4c26dcabdb
Garrett Dieckmann <g.dieckmann@f5.com>
2019-07-23 11:06:22 -0700

    checkpoint - adding in reassociation code
    Former-commit-id: e5b833d4dbcd74f74eb2cb7d20750d1d7a12a899

--- src/nodejs/providers/aws/cloud.js
+++ src/nodejs/providers/aws/cloud.js
@@ -18,7 +18,7 @@
 
 const AWS = require('aws-sdk');
 const CLOUD_PROVIDERS = require('../../constants').CLOUD_PROVIDERS;
-
+const util = require('../../util');
 const AbstractCloud = require('../abstract/cloud.js').AbstractCloud;
 
 class Cloud extends AbstractCloud {
@@ -66,7 +66,7 @@ class Cloud extends AbstractCloud {
             .then((eips) => {
                 this.logger.info('EIPS:');
                 this.logger.info(eips);
-                // TODO: shouldn't be global in future
+                // TODO: shouldn't be global in future. Or should it?
                 this.eips = eips.Addresses;
             })
             .then(() => {
@@ -79,34 +79,86 @@ class Cloud extends AbstractCloud {
                 this.logger.info('secondary private IP');
                 this.logger.info(data);
                 // for each EIP, we should re-assoc
-                return this._reassociateEIPs(data);
+                return this._generateNewEIPConfigs(data);
             })
             .then((data) => {
                 this.logger.info(data);
+                this._reassociateEIPs(data);
             })
             .catch(err => Promise.reject(err));
     }
 
     /**
-     * _reassociateEIPs();
-     * @param data - IPs for this instance
+     * Actually move the EIPs
+     * @param {Object} EIPConfig - EIP Configuration we should set
+     *
+     * @returns {Promise} - Resolves or rejects with status of moving the EIP
+     */
+    _reassociateEIPs(EIPConfig) {
+        // const disassociatePromises = [];
+        // const associatePromises = [];
+        Object.keys(EIPConfig).forEach((eip) => {
+            // disassociatePromises.push(util.retrier(this._disassociateIpAddress, eip.current.AssociationId));
+            this.logger.info('going to disassociate:');
+            this.logger.info(eip.current.AssociationId);
+        });
+    }
+
+    _disassociateIpAddress(associationIdToDisassociate) {
+        return new Promise((resolve, reject) => {
+            this.logger.info('disassociating');
+            const params = {
+                AssociationId: associationIdToDisassociate
+            };
+
+            this.ec2.disassociateAddress(params, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    _associateIpAddress() {
+
+    }
+
+    /**
+     * _generateNewEIPConfigs();
+     * @param privateInstanceIPs - IPs for this instance
      * // TODO: Not much thought here yet
      */
-    _reassociateEIPs(data) {
+    _generateNewEIPConfigs(privateInstanceIPs) {
+        const updatedState = {};
         this.eips.forEach((eip) => {
             // TODO: 'VIPS' should be a constant. Question: Should it be defined in the POST payload?
+            // TODO: How does Terraform do tagging? Would this change how we'd set our tags?
             const targetAddresses = eip.Tags.find(tag => tag.Key === 'VIPS').Value.split(',');
             targetAddresses.forEach((targetAddr) => {
                 this.logger.info('target:');
                 this.logger.info(targetAddr);
                 this.logger.info('IPs on instance:');
-                this.logger.info(data);
-                if (data.indexOf(targetAddr) !== -1) {
+                this.logger.info(privateInstanceIPs);
+                if (targetAddr in privateInstanceIPs) {
                     this.logger.info('found canditate to move!');
                     this.logger.info(`should move: ${eip.PublicIp} to ${targetAddr}, and off ${eip.PrivateIpAddress}`);
+                    updatedState[eip.PublicIp] = {
+                        target: {
+                            PrivateIpAddress: targetAddr,
+                            NetworkInterfaceId: privateInstanceIPs[targetAddr].NetworkInterfaceId
+                        },
+                        current: {
+                            PrivateIpAddress: eip.PrivateIpAddress,
+                            AssociationId: eip.AssociationId
+                        },
+                        allocationId: eip.allocationId
+                    };
                 }
             });
         });
+        return updatedState;
     }
 
     /**
@@ -128,12 +180,13 @@ class Cloud extends AbstractCloud {
         return new Promise((resolve, reject) => {
             this.ec2.describeNetworkInterfaces(params).promise()
                 .then((data) => {
-                    // resolve(data);
-                    const privateIps = [];
+                    const privateIps = {};
                     data.NetworkInterfaces.forEach((nic) => {
                         nic.PrivateIpAddresses.forEach((privateIp) => {
                             if (privateIp.Primary === false) {
-                                privateIps.push(privateIp.PrivateIpAddress);
+                                privateIps[privateIp.PrivateIpAddress] = {
+                                    NetworkInterfaceId: nic.NetworkInterfaceId
+                                };
                             }
                         });
                     });
