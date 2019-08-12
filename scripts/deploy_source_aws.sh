16a91a876927ab9e5473a904360714c00c9f9c66
Ganimat Singh <ganimat.singh@f5.com>
2019-08-12 16:05:13 -0700

    Add update json schema for failover routes
    Former-commit-id: 99d1ffa6ba67f86850d56e350225380a5957d784

--- scripts/deploy_source_aws.sh
+++ scripts/deploy_source_aws.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-FIRST_IP=$(cat test/functional/deployment/terraform/terraform.tfstate | jq '.resources[] | select(.name=="vm01") | .instances[].attributes.network_interface[1] | select(.name=="nic1") | .access_config[].nat_ip' | tr -d '"')
-SECOND_IP=$(cat test/functional/deployment/terraform/terraform.tfstate | jq '.resources[] | select(.name=="vm02") | .instances[].attributes.network_interface[1] | select(.name=="nic1") | .access_config[].nat_ip' | tr -d '"')
-PASSWORD=$(cat test/functional/deployment/terraform/terraform.tfstate | jq '.resources[] | select(.name=="admin_password") | .instances[].attributes.result' | tr -d '"')
+FIRST_IP=$(cat terraform.tfstate | jq '.resources[] | select(.name=="vm0") | .instances[].attributes.public_ip' | tr -d '"')
+SECOND_IP=$(cat terraform.tfstate | jq '.resources[] | select(.name=="vm1") | .instances[].attributes.public_ip' | tr -d '"')
+PASSWORD=$(cat terraform.tfstate | jq '.resources[] | select(.name=="admin_password") | .instances[].attributes.result' | tr -d '"')
 
 
-echo "FIRST IP:${FIRST_IP}"
+echo "FIRST IP: ${FIRST_IP}"
 echo "SECOND IP: ${SECOND_IP}"
 echo "PASSWORD: ${PASSWORD}"
 
