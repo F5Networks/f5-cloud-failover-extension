a6922eee542cdcc90bfaed9aea35d6d95ade5bb7
Garrett Dieckmann <g.dieckmann@f5.com>
2019-08-28 10:26:36 -0700

    first unit test
    Former-commit-id: 64383ad942e0f0dc0a9dcccec30da1156c90fd55

--- test/functional/tests/providers/aws/tests.js
+++ test/functional/tests/providers/aws/tests.js
@@ -8,9 +8,10 @@
 
 'use strict';
 
+const assert = require('assert');
+
 const AWS = require('aws-sdk');
 
-const utils = require('../../../../shared/util.js');
 const funcUtils = require('../../shared/util.js');
 
 const RETRIES = {
@@ -25,40 +26,45 @@ const dutSecondary = duts.filter(dut => !dut.primary)[0];
 
 const deploymentInfo = funcUtils.getEnvironmentInfo();
 
+// Helper functions
+function matchElasticIpToInstance(privateIp, instances) {
+    let match = false;
+    const primary = instances[dutPrimary.ip];
+
+    primary.NetworkInterfaces.forEach((nic) => {
+        nic.PrivateIpAddresses.forEach((pip) => {
+            if (pip.PrivateIpAddress === privateIp) {
+                match = true;
+            }
+        });
+    });
+
+    // assert
+    if (!match) {
+        assert.fail('ElasticIP does not match primary\'s secondary private IP');
+    }
+}
+
 describe('Provider: AWS', () => {
-    let primarySelfIps = [];
-    let secondarySelfIps = [];
-    let virtualAddresses = [];
+    const privateIpToInstance = {};
+
+    let ec2;
 
     before(function () {
         this.timeout(10000);
 
         AWS.config.update({ region: deploymentInfo.region });
-        this.ec2 = new AWS.EC2();
+        ec2 = new AWS.EC2();
 
-        return utils.getAuthToken(dutPrimary.ip, dutPrimary.username, dutPrimary.password)
+
+        return getEc2Instances({ Key: 'deploymentId', Value: deploymentInfo.deploymentId })
             .then((data) => {
-                const options = funcUtils.makeOptions({ authToken: data.token });
-                return utils.makeRequest(dutPrimary.ip, '/mgmt/tm/ltm/virtual-address', options);
-            })
-            .then((data) => {
-                virtualAddresses = data.items.map(i => i.address.split('/')[0]);
-                return utils.getAuthToken(dutPrimary.ip, dutPrimary.username, dutPrimary.password);
-            })
-            .then((data) => {
-                const options = funcUtils.makeOptions({ authToken: data.token });
-                return utils.makeRequest(dutPrimary.ip, '/mgmt/tm/net/self', options);
-            })
-            .then((data) => {
-                primarySelfIps = data.items.map(i => i.address.split('/')[0]);
-                return utils.getAuthToken(dutSecondary.ip, dutSecondary.username, dutSecondary.password);
-            })
-            .then((data) => {
-                const options = funcUtils.makeOptions({ authToken: data.token });
-                return utils.makeRequest(dutSecondary.ip, '/mgmt/tm/net/self', options);
-            })
-            .then((data) => {
-                secondarySelfIps = data.items.map(i => i.address.split('/')[0]);
+                Object.keys(data).forEach((key) => {
+                    privateIpToInstance[data[key].PublicIpAddress] = {
+                        InstanceId: key,
+                        NetworkInterfaces: data[key].NetworkInterfaces
+                    };
+                });
             })
             .catch(err => Promise.reject(err));
     });
@@ -68,6 +74,74 @@ describe('Provider: AWS', () => {
         });
     });
 
-    it('init test', () => {
+    // local functions
+    function getEc2Instances(tags) {
+        const params = {
+            Filters: [
+                {
+                    Name: `tag:${tags.Key}`,
+                    Values: [tags.Value]
+                }
+            ]
+        };
+
+        return new Promise((resolve, reject) => {
+            const instances = {};
+            ec2.describeInstances(params).promise()
+                .then((data) => {
+                    data.Reservations.forEach((reservation) => {
+                        const instance = reservation.Instances[0];
+                        instances[instance.InstanceId] = instance;
+                    });
+                    resolve(instances);
+                })
+                .catch(err => reject(err));
+        });
+    }
+
+    // function get
+
+    function getElasticIpPrivateAddress() {
+        const params = {
+            Filters: [
+                {
+                    Name: 'tag-key',
+                    Values: [
+                        'F5_CLOUD_FAILOVER_LABEL'
+                    ]
+                },
+                {
+                    Name: 'tag:deploymentId',
+                    Values: [deploymentInfo.deploymentId]
+                }
+            ]
+        };
+
+        return new Promise((resolve, reject) => {
+            ec2.describeAddresses(params).promise()
+                .then((data) => {
+                    const privateIp = data.Addresses[0].PrivateIpAddress;
+                    resolve(privateIp);
+                })
+                .catch(err => reject(err));
+        });
+    }
+
+    function checkElasticIP() {
+        return getElasticIpPrivateAddress()
+            .then((privateIp) => {
+                matchElasticIpToInstance(privateIp, privateIpToInstance);
+            })
+            .catch(err => Promise.reject(err));
+    }
+
+    // Functional tests!
+
+    // Test IP and Route failover
+    it('should check that Elastic IP is mapped to primary (vm0)', function () {
+        this.retries(RETRIES.LONG);
+
+        return checkElasticIP()
+            .catch(err => Promise.reject(err));
     });
 });
