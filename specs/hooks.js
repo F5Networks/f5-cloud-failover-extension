eb2efce7d30de263ec45312aaf2e6e43b649ed02
Ganimat Singh <ganimat.singh@f5.com>
2019-10-10 15:54:58 -0700

    skip trigger endpoint validation Run smart:run_functional_tests
    Former-commit-id: ff297e17da86073046ffa394598c28d2e5aea079

--- specs/hooks.js
+++ specs/hooks.js
@@ -6,6 +6,11 @@ hooks.before('/trigger > Running failover task state > 202 > application/json', 
     done();
 });
 
+hooks.after('/trigger > Running failover task state > 200 > application/json; charset=UTF-8', (transaction, done) => {
+    transaction.skip = true;
+    done();
+});
+
 hooks.after('/trigger > Running failover task state > 400 > application/json; charset=UTF-8', (transaction, done) => {
     transaction.skip = true;
     done();
