29f03767814f5033950024399cfbf4cdbdba6708
Ganimat Singh <ganimat.singh@f5.com>
2019-10-10 14:23:40 -0700

    Skip validation for triger endpoint
    Former-commit-id: c28b2c555825fc9ac5218f0a59ff930793c0900a

---
+++ specs/hooks.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line strict
+const hooks = require('hooks');
+
+hooks.before('/trigger > Running failover task state > 202 > application/json', (transaction, done) => {
+    transaction.skip = true;
+    done();
+});
+
+hooks.after('/trigger > Running failover task state > 400 > application/json; charset=UTF-8', (transaction, done) => {
+    transaction.skip = true;
+    done();
+});
