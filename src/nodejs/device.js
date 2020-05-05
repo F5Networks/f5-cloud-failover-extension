1e76248aac32afbe94ae54661c5fc1b4d93f2216
ngo <t.ngo@f5.com>
2020-05-05 11:31:51 -0700

    Fix bug virtual wildcard

--- src/nodejs/device.js
+++ src/nodejs/device.js
@@ -198,7 +198,16 @@ class Device {
         return this.getConfig([
             '/tm/ltm/virtual-address'
         ])
-            .then(results => Promise.resolve(results[0]))
+            .then((results) => {
+                const virtualAddresses = [];
+                results[0].forEach((result) => {
+                    if (result.address === 'any') {
+                        result.address = '0.0.0.0/0';
+                    }
+                    virtualAddresses.push(result);
+                });
+                return Promise.resolve(virtualAddresses);
+            })
             .catch(err => Promise.reject(err));
     }
 
