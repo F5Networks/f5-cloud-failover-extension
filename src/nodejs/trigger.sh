6de4d526423ddbc141a22fef0c3d924a43b91d48
Andrey Kashcheev <a.kashcheev@f5.com>
2021-09-22 21:07:04 +0000

    AUTOSDK-564: Add connection timeouts for curl triggers

--- src/nodejs/trigger.sh
+++ src/nodejs/trigger.sh
@@ -1,9 +1,9 @@
 i=0
 while [ $i -le 50 ]; do
-    if [[ "$(curl -u admin:admin -s -o /dev/null -w '%{http_code}' http://localhost:8100/mgmt/shared/cloud-failover/info)" == "200" ]]; then
+    if [[ "$(curl -u admin:admin --max-time 5 --connect-timeout 5 -s -o /dev/null -w '%{http_code}' http://localhost:8100/mgmt/shared/cloud-failover/info)" == "200" ]]; then
         break;
     fi
     sleep 3
     i=$(( $i + 1 ))
 done
-curl -u admin:admin -d {} -X POST http://localhost:8100/mgmt/shared/cloud-failover/trigger
+curl -u admin:admin --max-time 10 --connect-timeout 10 -d {} -X POST http://localhost:8100/mgmt/shared/cloud-failover/trigger
